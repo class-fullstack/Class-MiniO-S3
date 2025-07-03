1c018fe9f9977d0c30c05dcbe149facad684eac7
fdhhhdjd <nguyentientai10@gmail.com>
2025-07-03 08:52:17 +0700

    feat: new code

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // This is where you would import and use your MinIO client to interact with MinIO.
 // require("../../tests/minio.test");
