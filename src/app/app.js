b51c8a5ba25e98ec79eb24bef0dd4607dfdb5d54
fdhhhdjd <nguyentientai10@gmail.com>
2025-07-03 08:51:45 +0700

    feat: new code

--- src/app/app.js
+++ src/app/app.js
@@ -4,8 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+// This is where you would import and use your MinIO client to interact with MinIO.
 // require("../../tests/minio.test");
 
+// Importing routes for version 1 of the API
 app.use("/api/v1", require("./v1/routes"));
 
 module.exports = app;
